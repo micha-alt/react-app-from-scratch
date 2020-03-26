bf053337827b2ba06b3bf91a196982ede1b54009
Michael Alt <mail@michael-alt.info>
2020-03-26 20:25:07 +0100

    Handle JSX with Babel standalone

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 // Creating React element with text content
-const elt = React.createElement('h1', {className: 'text-2xl'}, 'Step 1: React Elements');
+const elt = <h1 className="text-2xl">Step 2: JSX (runtime compiled)</h1>;
 
 // Creating React element with nested elements
-const card = React.createElement('div', {className: 'max-w-sm mx-auto mt-8 rounded overflow-hidden shadow-lg'},
-  React.createElement('div', {className: 'px-6 py-4'}, [
-    React.createElement('div', {className: 'font-bold text-xl mb-2', key: 2}, 'Just Elements'),
-    React.createElement('p', {className: 'text-gray-700 text-base', key: 3}, `
-            React baut einen internen Elemente-Baum auf, der durch die ReactDOM Bibliothek in
-            einen Container gerendert wird. Dass heißt: jetzt werden die React-Elemente umgewandelt in
-            DOM-Knoten und nach einem Abgleich mit dem bestehenden DOM die Unterschiede/Änderungen angewandt.
-        `)
-  ])
+const card = (
+  <div className="max-w-sm mx-auto mt-8 rounded overflow-hidden shadow-lg">
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2">JSX ist eine JavaScript Extension</div>
+      <p className="text-gray-700 text-base">
+        Es ist also eine Vorbehandlung des Quelltextes für den Browser nötig. Dies kann
+        beispielsweise von <a href="https://babeljs.io/" className="underline">Babel</a> geleistet
+        werden. Es ist sogar eine Runtime-Compilierung möglich.
+      </p>
+    </div>
+  </div>
 );
 
 ReactDOM.render(elt, document.getElementById('root'));
