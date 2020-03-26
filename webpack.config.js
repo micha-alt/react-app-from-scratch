27a9e58bdc49c83951b111fc7b0e5c9ce2741b99
Michael Alt <mail@michael-alt.info>
2020-03-26 22:26:17 +0100

    Configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  // Default mode: development
+  // Can be overriden by commandline arg --mode=production
+  mode: "development",
+
+  // Entry Point - the main app file
+  entry: './src/app.js',
+
+  // Where and what to build
+  output: {
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "src/build")
+  },
+
+  // Default file extensions (may be omitted)
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  // Loaders (special handling for file types - beside just bundling ECMAScript)
+  module: {
+    rules: [
+      {
+        test: /\.js(x)?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
